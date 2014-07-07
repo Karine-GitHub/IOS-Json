9218ce9108be0110d2e27c43b2fa6a9add93e3db
admin <admin@admins-Mac-mini.local>
2014-07-07 16:32:11 +0200

    Initial Commit

---
+++ Test_Json/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test_Json
+//
+//  Created by admin on 7/7/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
